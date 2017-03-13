ddf10f570bcee9210caeca2b596beae8389ea544
Dmitriy211 <dmitriy.211@mail.ru>
2017-03-13 12:58:15 +0600

    calc

---
+++ Calculator/Brain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Brain
+    {
+        public double n1;
+        public double n2;
+        public double res;
+
+        public double Plus()
+        {
+            res = n1 + n2;
+            return res;
+        }
+
+        public double Minus()
+        {
+            res = n1 - n2;
+            return res;
+        }
+
+        public double Div()
+        {
+            res = n1 / n2;
+            return res;
+        }
+
+        public double Mul()
+        {
+            res = n1 * n2;
+            return res;
+        }        
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Brain brain = new Brain();
+        int k = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Num(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            textBox1.Text += btn.Text;
+        }
+
+        private void Op(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            k++;
+            brain.n1 = double.Parse(textBox1.Text);
+            textBox1.Text = "";
+
+            if (btn.Text.Equals("+") && k == 2)
+            {
+                k = 0;
+                brain.n2 = double.Parse(textBox1.Text);
+                textBox1.Text = brain.Plus().ToString();
+            }
+              
+        }
+    }
+}
